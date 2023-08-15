98de88903d6b59979cba3daf352d743bf04ba17b
Brandon Duong <brandon.duong@outlook.com>
2023-08-15 04:36:32 -0400

    added material ui

---
+++ components/layout.js
@@ -0,0 +1,46 @@
+import { Container, ThemeProvider, createTheme } from "@mui/material";
+import styles from "../styles/layout.module.css";
+export default function Layout({ children }) {
+  const theme = createTheme({
+    typography: {
+      fontFamily: `"Courier Prime", monospace`,
+    },
+  });
+
+  return (
+    <Container className={styles.container}>
+      <ThemeProvider theme={theme}>
+        <div className={styles.main}>
+          <div className={styles.inner}>{children}</div>
+        </div>
+      </ThemeProvider>
+
+      <style jsx>{`
+        main {
+        }
+      `}</style>
+      <style jsx global>{`
+        @import url("https://fonts.googleapis.com/css2?family=Courier+Prime:wght@400;700&display=swap");
+
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: Inter, -apple-system, BlinkMacSystemFont, Segoe UI,
+            Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
+            Helvetica Neue, sans-serif, "Courier Prime", monospace;
+          line-height: 1;
+          min-height: 100vh;
+          min-width: 320px;
+          overflow-x: hidden;
+          word-wrap: break-word;
+          background-color: #fafafa;
+        }
+
+        html {
+          font-size: 83.333333%;
+        }
+      `}</style>
+    </Container>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,53 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Layout from "../components/layout";
+import { Divider, Grid, Stack, Typography } from "@mui/material";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout>
       <Head>
         <title>Brandon Duong</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Stack
+        className={`${styles.header}`}
+        direction={"row"}
+        justifyContent={"space-between"}
+      >
+        <p>BEST VIEWED ON BROWSER</p>
+        <p>
+          <strong>UPDATED:</strong> 08/15/2023
+        </p>
+      </Stack>
+      <Typography variant="h2" className={styles.title} gutterBottom>
+        Employee Profile
+      </Typography>
+      <Typography variant="p" className={styles.name}>
+        Brandon Duong
+      </Typography>
+      <Stack
+        className={`${styles.position}`}
+        direction={"row"}
+        justifyContent={"space-around"}
+      >
+        <p>Employee ID: 511920518</p>
+        <p>Position Title: Software Engineer / Developer</p>
+        <p className={styles.classified}>
+          <strong>CLASSIFIED</strong>
+        </p>
+      </Stack>
 
-      <div className={styles.main}>
-        <div className={styles.inner}>
-          <h1 className={styles.title}>
-            Welcome to <a href="https://nextjs.org">Next.js!</a>
-          </h1>
-        </div>
-      </div>
-
-      <style jsx>{`
-        main {
-        }
-      `}</style>
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: Inter, -apple-system, BlinkMacSystemFont, Segoe UI,
-            Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
-            Helvetica Neue, sans-serif, "Courier Prime", monospace;
-          line-height: 1;
-          min-height: 100vh;
-          min-width: 320px;
-          overflow-x: hidden;
-          word-wrap: break-word;
-          background-color: #fafafa;
-        }
-
-        html {
-          font-size: 83.333333%;
-        }
-      `}</style>
-    </div>
+      <Grid container spacing={2}>
+        <Grid item md={6}>
+          <Typography variant="h6">IDENTIFICATION PHOTOGRAPH</Typography>
+          <Divider sx={{ borderBottomWidth: 3, borderColor: "black" }} />
+        </Grid>
+        <Grid item md={6}>
+          <Typography variant="h6">DEMOGRAPHIC INFORMATION</Typography>
+          <Divider sx={{ borderBottomWidth: 3, borderColor: "black" }} />
+        </Grid>
+      </Grid>
+    </Layout>
   );
 }
