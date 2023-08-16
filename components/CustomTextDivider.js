a32b69c2a53dae0eac8990564c957cbe5e3932f5
Brandon Duong <brandon.duong@outlook.com>
2023-08-16 06:27:15 -0400

    better responsiveness

--- components/CustomTextDivider.js
+++ components/CustomTextDivider.js
@@ -3,7 +3,7 @@ import { Divider, Grid, Typography } from "@mui/material";
 export default function CustomTextDivider({ text }) {
   return (
     <Grid container>
-      <Grid item xs={3} sm={3.75}>
+      <Grid item xs={0.5} sm={2} md={3.5}>
         <Divider
           sx={{
             borderBottomWidth: 3,
@@ -12,12 +12,12 @@ export default function CustomTextDivider({ text }) {
           }}
         />
       </Grid>
-      <Grid item xs={6} sm={4.5}>
+      <Grid item xs={11} sm={8} md={5}>
         <Typography variant="h4">
           <strong>{text}</strong>
         </Typography>
       </Grid>
-      <Grid item xs={3} sm={3.75}>
+      <Grid item xs={0.5} sm={2} md={3.5}>
         <Divider
           sx={{
             borderBottomWidth: 3,
